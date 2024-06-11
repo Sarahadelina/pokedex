e68ad0a47cb5aa8d901daf8545965eb613611952
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-06-11 15:41:16 +0200

    Add PokemonCard in components folder

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,13 @@
+
+function PokemonCard() {
+    return (
+
+        <figure>
+            <img src="https://www.123-stickers.com/7724-thickbox/autocollant-psykokwak-pokemon-054.jpg" alt="Psykokwak" />
+            <figcaption>Sarah attaque COIN-COIN !!!</figcaption>
+        </figure>
+
+    );
+}
+
+export default PokemonCard;
