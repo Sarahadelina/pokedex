a9777f9bfbfd227628ce040a3e3f58cd3ae816d6
sarah tuccitto <sarah.tuccitto@hotmail.fr>
2024-06-12 11:49:12 +0200

    make PokemonCard dynamic

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,28 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
 
-function PokemonCard() {
+const PokemonCard = () => {
+
+    const pokemon = pokemonList[1];
+
     return (
-
         <figure>
-            <img src="https://www.123-stickers.com/7724-thickbox/autocollant-psykokwak-pokemon-054.jpg" alt="Psykokwak" />
-            <figcaption>Sarah attaque COIN-COIN !!!</figcaption>
+            {
+                pokemon.imgSrc
+                    ? <img src={pokemon.imgSrc} alt={pokemon.name} />
+                    : <p>???</p>
+            }
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
-
     );
-}
+};
 
 export default PokemonCard;
